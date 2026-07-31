a101f0fe8acfe3c8d971147b9a7299d9590376c1
AndreyRamirez21 <ramirez.andrey@correounivalle.edu.co>
2026-07-30 22:13:38 -0500

    Agregado tiktok al footer

--- src/shared/ui/components/Footer.tsx
+++ src/shared/ui/components/Footer.tsx
@@ -26,6 +26,14 @@ function WhatsappIcon() {
   )
 }
 
+function TiktokIcon() {
+  return (
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6 0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64 0 3.33 2.76 5.7 5.69 5.7 3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48z" />
+    </svg>
+  )
+}
+
 export function Footer() {
   return (
     <footer>
@@ -59,6 +67,11 @@ export function Footer() {
               <a href="https://wa.me/573045507359" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp de Dalú" className="hover:text-primary transition-colors">
                 <WhatsappIcon />
               </a>
+              <a href="https://www.tiktok.com/@dalusiendotu?_r=1&_t=ZS-98TvCJVYyjx" target="_blank" rel="noopener noreferrer" aria-label="Tiktok de Dalú" className="hover:text-primary transition-colors">
+                <TiktokIcon />
+              </a>
+
+
             </div>
           </div>
 
